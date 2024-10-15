d04ec20864906f22b817743eda066d3bc53ded15
Fabrice <schaubfabrice@gmail.com>
2024-10-15 20:56:30 +0200

    added in a placeholder for now

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,32 @@
+import { Separator } from "@radix-ui/react-separator";
 import HeaderComponent from "../components/HeaderComponent";
 import HeroComponent from "../components/HeroComponent";
+import TerminalComponent from "@/components/TerminalComponent";
 
 export default function Home() {
   return (
     <main className="flex flex-col min-h-screen w-screen p-8 space-y-10">
       <HeaderComponent />
       <HeroComponent />
+
+      <Separator
+        orientation="horizontal"
+        className="w-full flex justify-center items-center"
+      >
+        <div className="w-32 h-1 bg-blue-600 rounded-full"></div>
+      </Separator>
+
+      <div className="flex flex-col items-center justify-center w-full space-y-6">
+        <h2 className="text-3xl font-bold text-center text-neutral-100">
+          Explore My Work
+        </h2>
+        <p className="text-lg text-center text-neutral-300 max-w-2xl">
+          Use the terminal to dive deeper into my projects and experience.
+          Prefer a traditional approach? No worries, you can also explore
+          everything without using the terminal.
+        </p>
+        <TerminalComponent />
+      </div>
     </main>
   );
 }
---
+++ src/components/TerminalComponent/TerminalComponent.tsx
@@ -0,0 +1,32 @@
+"use client";
+import { useEffect, useRef } from "react";
+
+const TerminalComponent = () => {
+  const terminalRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (terminalRef.current) {
+      terminalRef.current.scrollTop = terminalRef.current.scrollHeight;
+    }
+  }, []);
+
+  return (
+    <div className="flex flex-col items-center justify-center w-1/2 h-1/2 bg-zinc-900 shadow-lg rounded-lg">
+      <div className="flex items-center justify-between w-full p-3 bg-neutral-800 rounded-t-lg">
+        <h1 className="text-lg font-semibold text-neutral-100">Terminal</h1>
+        <div className="flex items-center space-x-2">
+          <div className="w-3 h-3 bg-red-500 rounded-full"></div>
+          <div className="w-3 h-3 bg-yellow-300 rounded-full"></div>
+          <div className="w-3 h-3 bg-green-500 rounded-full"></div>
+        </div>
+      </div>
+
+      <div
+        ref={terminalRef}
+        className="flex flex-col flex-start w-full h-96 p-3 space-y-2 overflow-y-auto bg-zinc-900 rounded-b-lg no-scrollbar"
+      ></div>
+    </div>
+  );
+};
+
+export default TerminalComponent;
