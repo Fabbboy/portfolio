699daae49cac5b8d82da15b937bbf8202c6e895a
Fabrice <schaubfabrice@gmail.com>
2024-10-16 19:21:58 +0200

    trying different sizes

--- src/components/TerminalComponent/TerminalComponent.tsx
+++ src/components/TerminalComponent/TerminalComponent.tsx
@@ -11,7 +11,7 @@ const TerminalComponent = () => {
   }, []);
 
   return (
-    <div className="flex flex-col items-center justify-center w-9/12 h-1/2 bg-zinc-900 shadow-lg rounded-lg">
+    <div className="flex flex-col items-center justify-center w-11/12 md:w-7/12 h-1/2 bg-zinc-900 shadow-lg rounded-lg">
       <div className="flex items-center justify-between w-full p-3 bg-neutral-800 rounded-t-lg">
         <h1 className="text-lg font-semibold text-neutral-100">Terminal</h1>
         <div className="flex items-center space-x-2">
