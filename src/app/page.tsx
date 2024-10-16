0da69692c4e1eb424967a3c28b9fbabd2961695f
Fabrice <schaubfabrice@gmail.com>
2024-10-16 20:40:08 +0200

    trying responsiveness

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,6 +14,12 @@ export default function Home() {
       >
         <div className="w-32 h-1 bg-blue-600 rounded-full"></div>
       </Separator>
+
+      <section className="flex flex-col items-center justify-center w-full h-full">
+        <h2 className="text-2xl font-semibold text-center text-neutral-100">
+          Under Construction
+        </h2>
+      </section>
     </main>
   );
 }
