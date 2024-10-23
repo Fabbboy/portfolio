80435f48af253087734641937ed68df4a0417a90
Fabrice <fabrice.schaub@swisscom.com>
2024-10-23 11:09:16 +0200

    removes horizontal scrollbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased h-full w-full bg-neutral-900`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased h-full w-full bg-neutral-900 overflow-x-hidden`}
       >
         <div className="relative h-full w-full">
           <div className="absolute inset-0 h-full w-full bg-white bg-[linear-gradient(to_right,#80808012_1px,transparent_1px),linear-gradient(to_bottom,#80808012_1px,transparent_1px)] bg-[size:24px_24px]">
