2ec58eb51040a4f48bdecddf3aabec3b4321b688
Fabrice <fabrice.schaub@swisscom.com>
2024-10-24 15:12:53 +0200

    some media query

--- src/components/SectionStartComponent/SectionStartComponent.tsx
+++ src/components/SectionStartComponent/SectionStartComponent.tsx
@@ -5,9 +5,13 @@ type Props = {
 
 const SectionStartComponent: React.FC<Props> = ({ title, description }) => {
   return (
-    <div className="flex flex-col items-center space-y-4">
-      <p className="text-3xl font-bold text-blue-400">{title}</p>
-      <span className="text-blue-200 text-center">{description}</span>
+    <div className="flex flex-col items-center space-y-4 p-4 md:p-8">
+      <p className="text-2xl md:text-4xl lg:text-5xl font-bold text-blue-400 text-center">
+        {title}
+      </p>
+      <span className="text-blue-200 text-center text-base md:text-lg lg:text-xl max-w-screen-md">
+        {description}
+      </span>
     </div>
   );
 };
