6c026ea6516af6b83e9c994a7edb0fdd9b22c4cc
Fabrice <fabrice.schaub@swisscom.com>
2024-10-25 14:04:02 +0200

    media query

--- src/components/TestimonialListComponent/TestimonialListComponent.tsx
+++ src/components/TestimonialListComponent/TestimonialListComponent.tsx
@@ -30,18 +30,19 @@ const TestimonialListComponent = () => {
   }, [host]);
 
   return (
-    <div className="container mx-auto px-4 py-12">
+    <div className="container mx-auto px-2 sm:px-4 py-8 sm:py-12">
       <SectionStartComponent
         title="Testimonials"
         description="Here you can find some testimonials from people I have worked with."
       />
-      <div className="mt-12 grid gap-6 md:grid-cols-2 lg:grid-cols-3">
+      <div className="mt-8 sm:mt-12 grid gap-6 md:grid-cols-2 lg:grid-cols-3">
         {testimonials.map((testimonial, index) => (
           <TestimonialComponent key={index} testimonial={testimonial} />
         ))}
       </div>
     </div>
   );
+  
 };
 
 export default TestimonialListComponent;
