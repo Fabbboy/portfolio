88b4a3b58b731ea0e533f8cfb8e027cc77b5841b
Fabrice <fabrice.schaub@swisscom.com>
2024-10-26 07:53:18 +0200

    planing

--- src/components/TestimonialListComponent/TestimonialListComponent.tsx
+++ src/components/TestimonialListComponent/TestimonialListComponent.tsx
@@ -31,21 +31,25 @@ export default function TestimonialListComponent() {
   }, [host]);
 
   return (
-    <div className="container mx-auto px-2 sm:px-4 py-8 sm:py-12">
-      <SectionStartComponent
-        title="Testimonials"
-        description="Here you can find some testimonials from people I have worked with."
-      />
-      <div className="mt-8 sm:mt-12 flex flex-wrap justify-center gap-6">
-        {testimonials.map((testimonial, index) => (
-          <div
-            key={index}
-            className="w-full sm:w-[calc(50%-12px)] lg:w-[calc(33.333%-16px)] flex justify-center"
-          >
-            <TestimonialComponent testimonial={testimonial} />
+    <>
+      {testimonials.length > 0 && (
+        <div className="container mx-auto px-2 sm:px-4 py-8 sm:py-12">
+          <SectionStartComponent
+            title="Testimonials"
+            description="Here you can find some testimonials from people I have worked with."
+          />
+          <div className="mt-8 sm:mt-12 flex flex-wrap justify-center gap-6">
+            {testimonials.map((testimonial, index) => (
+              <div
+                key={index}
+                className="w-full sm:w-[calc(50%-12px)] lg:w-[calc(33.333%-16px)] flex justify-center"
+              >
+                <TestimonialComponent testimonial={testimonial} />
+              </div>
+            ))}
           </div>
-        ))}
-      </div>
-    </div>
+        </div>
+      )}
+    </>
   );
 }
