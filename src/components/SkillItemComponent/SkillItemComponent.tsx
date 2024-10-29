d292c05fb45ae857ab94788622bd8364580e1272
Fabrice <schaubfabrice@gmail.com>
2024-10-29 15:44:24 +0100

    adjustments

--- src/components/SkillItemComponent/SkillItemComponent.tsx
+++ src/components/SkillItemComponent/SkillItemComponent.tsx
@@ -7,7 +7,7 @@ type Props = {
 
 const SkillItemComponent: React.FC<Props> = ({ skillItem }) => {
   return (
-    <div className="flex flex-row items-center justify-center min-w-[120px] max-w-[160px] p-4 bg-neutral-800 border border-neutral-700 rounded-lg space-x-4">
+    <div className="flex flex-row items-center justify-center min-w-[120px] max-w-[160px] h-[60px] p-4 bg-neutral-800 border border-neutral-700 rounded-lg space-x-4">
       <div className="flex flex-col items-center justify-center">
         <Image
           src={skillItem.icon}
